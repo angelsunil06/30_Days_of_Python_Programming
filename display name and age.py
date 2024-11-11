683c998dd38a0d6e70d0f6e4d710f0e574030c82
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-11-11 21:30:32 +0530

    Add files via upload

---
+++ display name and age.py
@@ -0,0 +1,8 @@
+"""
+Author: Daiji Kuriakose
+Date  :11-11-2024
+Title :Python program to display name and age
+"""
+name=input("Enter your name")
+age=int(input("Enter your age"))
+print(f"Hello {name}! You are {age} years old")
---
+++ wellcome to python.py
@@ -0,0 +1,7 @@
+"""
+Author: Daiji Kuriakose
+Date  :11-11-2024
+Title :Python program to printthe string "welcome to programming with python"
+"""
+string=("welcome to programming with python")
+print(string)
