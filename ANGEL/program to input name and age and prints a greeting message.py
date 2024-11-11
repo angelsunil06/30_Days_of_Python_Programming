4d6a366cb323276ecbd04350b4c16fbcc9f55c49
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-11-11 23:25:44 +0530

    Add files via upload

---
+++ ANGEL/program to input name and age and prints a greeting message.py
@@ -0,0 +1,9 @@
+'''
+author:angel
+date:11-11-24
+title:Program to input name and age and print a greeting message
+
+'''
+name=input('enter your name:')
+age=int(input('enter your age:'))
+print('hello',name,'! you are',age,'years old')
