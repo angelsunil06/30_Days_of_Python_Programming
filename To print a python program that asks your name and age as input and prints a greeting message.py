fb0cb11eeae10476c944a25184c0663e4a56e362
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-11-12 22:19:50 +0530

    Delete To print a python program that asks your name and age as input and prints a greeting message.py

--- To print a python program that asks your name and age as input and prints a greeting message.py
+++
@@ -1,5 +0,0 @@
-#2 To print a python program that asks your name and age as input and prints a greeting message
-
-name=input('Enter your name:')
-age=int(input('Enter your age:'))
-print('Hello', name,'! you are', age, 'years old')
