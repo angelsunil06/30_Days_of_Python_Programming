a5ea41672a0f08a9664068a5ebede1a67cb3d688
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-11-12 23:09:22 +0530

    Add files via upload

---
+++ ANGEL/day2 activity2 to check whether a password is valid or not.py
@@ -0,0 +1,13 @@
+'''
+author:angel
+date:12-11-24
+program to check whether a password is valid or not
+
+'''
+
+password=input('enter the password:')
+if (len(password)<=6):
+    print('password is invalid')
+else:
+   print('password is valid')
+    
