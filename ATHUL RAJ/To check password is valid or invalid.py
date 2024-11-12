a9bc6a4eb7acc6b7ce9e4282f0c76593e338b6fb
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-11-12 21:06:40 +0530

    Add files via upload

---
+++ ATHUL RAJ/To check password is valid or invalid.py
@@ -0,0 +1,11 @@
+#created by athul raj
+#date: 11-11-2024
+# to check password is invalid or not
+string=input("Enter the password:")
+if len(string)>=6:
+    print("the password is valid")
+else:
+    print("the password is invalid")
+      
+
+        
