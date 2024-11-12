a24b17e2e902f119706ce305ec223144bc58182b
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-11-12 22:20:57 +0530

    Delete wellcome to python.py

--- wellcome to python.py
+++
@@ -1,7 +0,0 @@
-"""
-Author: Daiji Kuriakose
-Date  :11-11-2024
-Title :Python program to printthe string "welcome to programming with python"
-"""
-string=("welcome to programming with python")
-print(string)
