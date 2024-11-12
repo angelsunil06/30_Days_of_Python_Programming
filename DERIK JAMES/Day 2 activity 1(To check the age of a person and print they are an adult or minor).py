1022f15d9394ce716ba8902c8da1e7021efd6537
derikjames636 <derikjames2028@cs.sjcetpalai.ac.in>
2024-11-12 22:02:42 +0530

    Add files via upload

---
+++ DERIK JAMES/Day 2 activity 1(To check the age of a person and print they are an adult or minor).py
@@ -0,0 +1,7 @@
+'''To check the age of a person and print they are an adult or minor'''
+
+age=int(input('Enter your age:'))
+if age>=18:
+    print('You are a major')
+else :
+    print('You are a minor')
---
+++ DERIK JAMES/Day 2 activity 2 checking password.py
@@ -0,0 +1,14 @@
+
+#To print if a password is valid or not. If the length of password is less than 6 characters then print the password is invalid,else print the password is valid
+
+password=input('Enter the password')
+
+if len(password)>=6:
+    print('password is valid')
+else:
+    print('password is invalid')
+
+
+    
+
+
