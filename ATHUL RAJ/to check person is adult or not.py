e75fb67aa791ac07716410b550bf0b646693059b
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-11-12 20:46:33 +0530

    Add files via upload

---
+++ ATHUL RAJ/to check person is adult or not.py
@@ -0,0 +1,9 @@
+#created by athul raj
+#date: 11-11-2024
+# to check person is adult or minor
+age=int(input("Enter your age:"))
+if age>18:
+    print('you are an adult')
+else:
+    print('you are a minor')
+        
