adc8bf1f2a5c9a89b6e06e16a8daaaf3895eff48
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-11-12 23:07:36 +0530

    Add files via upload

---
+++ ANGEL/day2 activity1 to check whether a person is minor or adult.py
@@ -0,0 +1,11 @@
+'''
+author:angel
+date:12-11-24
+program to input age of a person and to check whether they are  major or adult
+
+'''
+age=int(input('enter your age:'))
+if age<18:
+    print('you are a minor')
+else:
+    print('you are an adult')
