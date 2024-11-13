ee37bdde8fc39ec76edb95fc605414fa744dd558
derikjames636 <derikjames2028@cs.sjcetpalai.ac.in>
2024-11-13 18:46:15 +0530

    Add files via upload

---
+++ DERIK JAMES/Day 3 activity 2 program to print sum of numbers from 1 to 15.py
@@ -0,0 +1,7 @@
+#To print sum of numbers from 1 to 15
+
+total=0
+
+for num in range(1,16):
+    total += num
+print(total)
---
+++ DERIK JAMES/day 3 activity 1 swapping values.py
@@ -0,0 +1,16 @@
+# program to swap the values of two variables.print the values before and after swapping.
+
+
+
+num1=int(input('Enter a number:'))
+num2=int(input('Enter another number:'))
+ 
+print(num1)
+print(num2)
+
+temp=num1
+num1=num2
+num2=temp
+
+print(num1)
+print(num2)
