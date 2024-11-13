81cf80e8a94253f36f8f1cdad22472b68075f60e
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-11-13 23:12:23 +0530

    Add files via upload

---
+++ ANGEL/Day 3 activity1 program to swap the value of two variables.py
@@ -0,0 +1,17 @@
+'''
+author:angel
+date:13-12-24
+program to swap the value of two variables
+
+'''
+x=int(input('enter a number:'))
+y=int(input('enter a number:'))
+print('values before swapping')
+print('x=',x)
+print('y=',y)
+z=x
+x=y
+y=z
+print('values after swapping')
+print('x=',x)
+print('y=',y)
