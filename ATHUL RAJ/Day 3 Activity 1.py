9b6f27f8b4617f00a19d76a4782a8a5168595ca2
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-11-13 19:20:38 +0530

    Add files via upload

---
+++ ATHUL RAJ/Day 3 Activity 1.py
@@ -0,0 +1,12 @@
+#created by athul raj
+#date :13-11-2024
+#To swap values of variables 
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+print("Values before swapping:")
+print("Number1=",num1)
+print("Number2=",num2)
+print("Values after swapping:")
+print("Number1=",num2)
+print("Number2=",num1)
+
