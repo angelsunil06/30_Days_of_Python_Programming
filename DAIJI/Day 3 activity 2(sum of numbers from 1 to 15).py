f0d595d4ae2f10874d03a710ee3ee2ed80aa5b54
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-11-13 21:13:26 +0530

    Add files via upload

---
+++ DAIJI/Day 3 activity 2(sum of numbers from 1 to 15).py
@@ -0,0 +1,11 @@
+"""
+Author: Daiji Kuriakose
+Date  :13-11-2024
+Title :Python program to print the sum of numbers from 1 to 15
+"""
+sum=0
+i=0
+while(i<=15):
+    sum=sum+i
+    i=i+1
+print(f"The sum of numbers 1 to 15 is:{sum}")
