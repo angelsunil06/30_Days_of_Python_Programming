a3cff02e1c7a263df9abb417ac27e328e473a394
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-11-13 20:57:30 +0530

    Add files via upload

---
+++ DAIJI/Day 3 activity 1(swap the values of two variables).py
@@ -0,0 +1,13 @@
+"""
+Author: Daiji Kuriakose
+Date  :13-11-2024
+Title :Python program to swap the values of two variables
+"""
+number_1=int(input("Enter first number:"))
+number_2=int(input("Enter second number:"))
+print("Values before swapping")
+print(f"Number1={number_1}")
+print(f"Number2={number_2}")
+print("Values after swapping")
+print(f"number1={number_2}")
+print(f"number2={number_1}")
