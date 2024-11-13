b22c337a28d44e1a877dd647e6cd7a88d0c18648
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-11-13 23:18:44 +0530

    Add files via upload

---
+++ ATHUL RAJ/Day 3 activity 2(To print sum of numbers from 1 to 15).py
@@ -0,0 +1,7 @@
+#created by athul raj
+#date:13-11-2024
+#To print sum of numbers from 1 to 15
+total_sum=0
+for number in range(1, 16):
+    total_sum += number
+print("The total sum of numbers from 1 to 15 is:",total_sum)    
