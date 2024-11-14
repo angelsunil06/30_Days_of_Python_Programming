3458bc897e886f1a9d5c0525bbeae4375dc84c97
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-11-14 22:36:44 +0530

    Add files via upload

---
+++ DAIJI/Day 4 activity 2(average of four numbers inputted by the user).py
@@ -0,0 +1,11 @@
+"""
+Author: Daiji Kuriakose
+Date:14-11-2024
+Title:Python program to find the average of four numbers inputted by the user
+"""
+number_1=int(input("Enter the first number"))
+number_2=int(input("Enter the second number"))
+number_3=int(input("Enter the third number"))
+number_4=int(input("Enter the fourth number"))
+average=(number_1+number_2+number_3+number_4)/4
+print(f"The average of four numbers is:{average}")
