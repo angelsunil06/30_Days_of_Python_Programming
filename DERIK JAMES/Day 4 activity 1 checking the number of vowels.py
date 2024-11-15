e8b3a9b5d62f264c740723b5d94e24c610dfdfd3
derikjames636 <derikjames2028@cs.sjcetpalai.ac.in>
2024-11-15 06:10:33 +0530

    Add files via upload

---
+++ DERIK JAMES/Day 4 activity 1 checking the number of vowels.py
@@ -0,0 +1,13 @@
+#checking number of vowels
+
+string_1=input('Enter a string')
+vowels='aeiouAEIOU'
+vowels_count=0
+consonents_count=0
+for char in string_1:
+    if char in vowels:
+        vowels_count+=1
+    else :
+       consonents_count+=1
+print(f'number of vowels in the given string' , {vowels_count})
+print(f'the number of consonents in the given string' , {consonents_count})
