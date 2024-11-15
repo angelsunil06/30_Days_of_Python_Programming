092e935633d9a038be287bbaba739fbc7db9f2ad
derikjames636 <derikjames2028@cs.sjcetpalai.ac.in>
2024-11-15 06:11:06 +0530

    Add files via upload

---
+++ DERIK JAMES/Day 4 activity 4 To print average of 4 numbers.py
@@ -0,0 +1,9 @@
+#to find average of four numbers
+
+num1=int(input('Enter first number'))
+num2=int(input('Enter second number'))
+num3=int(input('Enter third number'))
+num4=int(input('Enter fourth number'))
+
+average=(num1+num2+num3+num4)/4
+print(average)
