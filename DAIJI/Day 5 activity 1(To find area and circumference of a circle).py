1933d2e21799291b98f440947c35be203da74aff
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-11-15 21:47:16 +0530

    Add files via upload

---
+++ DAIJI/Day 5 activity 1(To find area and circumference of a circle).py
@@ -0,0 +1,10 @@
+"""
+Author: Daiji Kuriakose
+Date:15-11-2024
+Title:Python program to find the area and circumference of a circle
+"""
+radius=float(input("Enter tha radius of the circle"))
+area=(3.14*radius*radius)
+circumference=(2*3.14*radius)
+print(f"The area of the circle is {area} square centimeters")
+print(f"The circumference of a circle is: {circumference} centimeters")
