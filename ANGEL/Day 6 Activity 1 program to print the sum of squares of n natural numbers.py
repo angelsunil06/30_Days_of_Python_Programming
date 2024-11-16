8015568992387b7e00b50ec19f2824606b716b5e
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-11-16 19:41:30 +0530

    Day 6 Activity 1 program to print the sum of squares of n natural numbers.py

--- ANGEL/Day 6 Activity 1 program to print the sum of squares of n natural numbers.py
+++ ANGEL/Day 6 Activity 1 program to print the sum of squares of n natural numbers.py
@@ -6,6 +6,6 @@ program to print the sum of squares of n natural numbers
 '''
 limit=int(input('enter no of terms:'))
 sum=0
-for i in range(limit+1):
+for i in range(1,limit+1):
 	sum+=i*i
 print (sum)
