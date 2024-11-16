fe191c88298db5ff9c3e340aa92cec4f7e39c902
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-11-16 19:58:07 +0530

    Add files via upload

---
+++ ATHUL RAJ/Day 5 activity 1(To print area and circumference of a circle).py
@@ -0,0 +1,9 @@
+#created by athul raj
+#date:14-11-2024
+#To print area and circumference of a circle
+num=int(input("Enter the radius of a circle:"))
+area=3.14*num**2
+circum=2*3.14*num
+print("The area of a circle is:",area)
+print("The circumference of a circle is :",circum)
+    
