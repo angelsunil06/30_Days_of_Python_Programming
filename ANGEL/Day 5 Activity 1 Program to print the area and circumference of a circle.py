78d67a2bcce45d464f70d6007b3859c4c873eb87
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-11-16 19:38:31 +0530

    Add files via upload

---
+++ ANGEL/Day 5 Activity 1 Program to print the area and circumference of a circle.py
@@ -0,0 +1,11 @@
+'''
+author:angel
+date:15-11-24
+program to find the area of the circle
+
+'''
+radius=float(input('enter radius of the circle:'))
+area= 3.14*radius*radius
+circumference= 2*3.14*radius
+print('the area of the circle is:',area)
+print('the circumference of the circle is:',circumference)
