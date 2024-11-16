9a7cd5cac7df2e955840cd70eb2d42d408d44e44
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-11-16 19:44:44 +0530

    Add files via upload

---
+++ ATHUL RAJ/Day 4 activity 2 (To print average of four numbers).py
@@ -0,0 +1,11 @@
+#created by athul raj
+#date:14-11-2024
+#To print average of four numbers inputted by user
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+num3=int(input("Enter third number:"))
+num4=int(input("Enter fourth number:"))
+total_sum=num1+num2+num3+num4
+avg=total_sum/4
+print("The average value of 4 numbers is:",avg)
+    
