47430dfe355910978485108e6ebb51812a556071
derikjames636 <derikjames2028@cs.sjcetpalai.ac.in>
2024-11-17 12:06:03 +0530

    Add files via upload

---
+++ DERIK JAMES/Day 5 activity 1 To find area and circumference of a circle.py
@@ -0,0 +1,12 @@
+#To find circumfrence of a circle
+
+r=float(input('Enter radius'))
+
+import math
+
+C=2*math.pi*r
+print('Circumfrence of the circle is equal to :',C)
+
+#To find area of a circle
+A=math.pi*r**2
+print('Area of the circle is equal to :',A)
---
+++ DERIK JAMES/Day 6 activity 1 sum of squares of n natural numbers.py
@@ -0,0 +1,7 @@
+#sum of squares of n natural numbers
+
+import math
+n=int(input('Enter a number'))
+
+sum_of_num=(n*(n+1)*(2*n+1))//6
+print(sum_of_num)
