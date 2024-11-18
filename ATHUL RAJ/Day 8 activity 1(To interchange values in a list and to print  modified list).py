905aeb385d145a29fb8b8a92574b667184e63b8d
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-11-18 22:01:51 +0530

    Add files via upload

---
+++ ATHUL RAJ/Day 8 activity 1(To interchange values in a list and to print  modified list).py
@@ -0,0 +1,11 @@
+#created by athul raj
+#date:18-11-2024
+#To interchange values in a list
+list_num=[10,20,30,40]
+temp=list_num[0]
+list_num[0]=list_num[-1]
+list_num[-1]=temp
+print("The modified list is :",list_num)
+
+
+    
