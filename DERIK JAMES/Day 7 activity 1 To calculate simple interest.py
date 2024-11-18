e9c9d34a72e6ebf0e435a1135ac06feaa7fb3fdf
derikjames636 <derikjames2028@cs.sjcetpalai.ac.in>
2024-11-18 19:25:51 +0530

    Add files via upload

---
+++ DERIK JAMES/Day 7 activity 1 To calculate simple interest.py
@@ -0,0 +1,9 @@
+#To calculate simple intrest
+
+P=int(input('Enter principle amount '))
+r=int(input('Enter interest rate per annum'))
+T=int(input('Enter time in years '))
+
+#simple interest is equal to
+SI=P*r*T/100
+print(SI)
