1ab5f2120005be3fdaf1715a7b2f3dcd6041b7c1
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-11-18 22:26:42 +0530

    Add files via upload

---
+++ DAIJI/Day 8 activity 1 (create a list and swap the element).py
@@ -0,0 +1,8 @@
+"""
+Author: Daiji Kuriakose
+Date:18-11-2024
+Title:Python program to create a list and swap the element
+"""
+list=[10,20,30,40]
+list[0],list[-1]=list[-1],list[0]
+print(f"The modified list: {list}")
