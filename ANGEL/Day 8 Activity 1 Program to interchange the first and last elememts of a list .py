6be7d8731425f3883dfeaa9454c5f0d869fb9f8f
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-11-18 20:02:17 +0530

    Add files via upload

---
+++ ANGEL/Day 8 Activity 1 Program to interchange the first and last elememts of a list .py
@@ -0,0 +1,8 @@
+'''
+author:Angel
+date:18-11-24
+program to interchange first and last elements of a list
+'''
+list1=[10,20,30,40]
+list1[0],list1[-1]=list1[-1],list1[0]
+print('the modified list is',list1)
