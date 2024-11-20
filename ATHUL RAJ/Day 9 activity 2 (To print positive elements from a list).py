5958fee15427e6738f4376765aad676cfd783519
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-11-20 23:26:30 +0530

    Add files via upload

---
+++ ATHUL RAJ/Day 9 activity 2 (To print positive elements from a list).py
@@ -0,0 +1,10 @@
+#created by athul raj
+#date:19-11-2024
+#To input positive and negative elements in a list and print positive elements in a list
+list=[-18,-37,-26,-30,15,38,47,20,47,38]
+print(f"The positive elements are :")
+for num in list:
+    if num>0:
+     print(num)        
+
+    
