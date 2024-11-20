daf00ab5b3d93171693e1cd6c941a9559ca33bf6
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-11-20 23:18:27 +0530

    Add files via upload

---
+++ ATHUL RAJ/Day 9 activity 1 (To check whether given element is in the list).py
@@ -0,0 +1,12 @@
+#created by athul raj
+#date:19-11-2024
+#To check whether given element is in a list
+list=[10,20,30,40,50]
+number=int(input("Enter a number :"))
+if number in list:
+    print(f"The element {number} exist in the list")
+else:
+    print(f"The element {number} not exist in the list")    
+
+
+    
