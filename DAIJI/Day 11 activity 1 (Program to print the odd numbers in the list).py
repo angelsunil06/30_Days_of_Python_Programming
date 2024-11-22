f3432cd30ce1895965519970103032db08dd9821
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-11-22 22:02:55 +0530

    Add files via upload

---
+++ DAIJI/Day 11 activity 1 (Program to print the odd numbers in the list).py
@@ -0,0 +1,10 @@
+"""
+Author: Daiji Kuriakose
+Date:21-11-2024
+Title:Python Program to print the odd numbers in the list
+"""
+list=[1,2,3,6,7,8,9,15]
+for num in list:
+    if num%2!=0:
+     print(num,end=" ")
+print()
