50e5f5f08fbeb71deba1cabdec00e2936e6845b3
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-11-24 23:37:08 +0530

    Add files via upload

---
+++ ATHUL RAJ/Day 11 activity 1(To input list and to print odd numbers in a list).py
@@ -0,0 +1,11 @@
+#created by athul raj
+#date:21-11-2024
+#To input a list and to print odd numbers in a list
+list=[15,23,20,35,17,29,12,24,8,4]
+for num in list:
+    if num%2!=0:
+     print(num,end=" ")
+print()    
+
+
+    
