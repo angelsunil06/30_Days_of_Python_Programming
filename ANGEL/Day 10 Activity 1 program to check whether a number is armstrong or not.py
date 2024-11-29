bd38366539650f0b7cd2fd7d00993aa57a46ee4f
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-11-29 23:37:31 +0530

    Add files via upload

---
+++ ANGEL/Day 10 Activity 1 program to check whether a number is armstrong or not.py
@@ -0,0 +1,17 @@
+'''
+author:Angel
+date:21-11-24
+program to check whether a number is armstrong or not
+
+'''
+number=int(input("Enter a number:"))
+armstrong=number
+sum=0
+while number>0:
+    remainder=number%10
+    sum+=remainder*remainder*remainder
+    number=number//10
+if sum==armstrong:
+    print("The number is armstrong")
+else:
+    print("The number is not armstrong")
