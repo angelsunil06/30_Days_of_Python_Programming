1a502e296ad0622ae295f88170ea75f236280e98
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-11-29 23:25:05 +0530

    Add files via upload

---
+++ ANGEL/Day 9 Activity 1 program to check whether a number exists in a list or not.py
@@ -0,0 +1,6 @@
+list=[10,20,30,40,50]
+number=int(input("enter a number: "))
+if number in list:
+    print("The element" ,number,"exist in the list")
+else:
+    print("The element", number,"does not exist in the list")
