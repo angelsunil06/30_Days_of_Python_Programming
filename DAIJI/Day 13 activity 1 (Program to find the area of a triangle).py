d028a8969ff810cb6c84041fc7b20e420c1f885c
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-12-01 23:58:50 +0530

    Add files via upload

---
+++ DAIJI/Day 13 activity 1 (Program to find the area of a triangle).py
@@ -0,0 +1,9 @@
+"""
+Author: Daiji Kuriakose
+Date:1-12-2024
+Title:Python Program to find the area of a triangle
+"""
+base=int(input("Enter the base length of the triangle: "))
+height=int(input("Enter the height of the triangle: "))
+area=base*height/2
+print(area)
