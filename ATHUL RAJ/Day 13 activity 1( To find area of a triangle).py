8ea7e04e1e8d4bbc2cda12f29ba3ef92648b0fab
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-12-02 01:21:22 +0530

    Add files via upload

---
+++ ATHUL RAJ/Day 13 activity 1( To find area of a triangle).py
@@ -0,0 +1,7 @@
+#created by athul raj
+#date:1-12-2024
+#program to print area of triangle
+base=int(input("Enter base length of triangle :"))
+height=int(input("Enter height of triangle :"))
+area=base*height*1/2
+print(area)
